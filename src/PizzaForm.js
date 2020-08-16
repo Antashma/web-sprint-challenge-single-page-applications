541bc46f0fd0b6a32313a0d96e09008c069c956e
Samantha Goodman <slgoodman89@gmail.com>
2020-08-16 14:12:57 -0400

    fixed error with validation

--- src/PizzaForm.js
+++ src/PizzaForm.js
@@ -17,15 +17,10 @@ export default function PizzaForm () {
     const [formState, setFormState] = useState({...blankForm})
     const [btnDisabled, setBtnDisabled] = useState(true)
     const [errorState, setErrorState] = useState({...blankForm})
+    const [passState, setPassState] = useState({...blankForm})
     //console.log('form', formState, 'btn', btnDisabled, 'error', errorState)
 
-    //change handler
-    const handleChanges = (event) => { 
-        event.persist() 
-        validateChange(event)
-        setFormState({...formState, [event.target.name]:event.target.type === 'checkbox' ? event.target.checked : event.target.value})
-        console.log('input changed!', formState)
-    }
+
 
     //submit
     const submitOrder = (event) => {
@@ -35,8 +30,8 @@ export default function PizzaForm () {
 
     //form schema
     const formSchema = yup.object().shape({
-        name: yup.string().min('limit: 2'),
-        size: yup.string().oneOf(['small', 'medium', 'large'], 'Please select a size!'),
+        name: yup.string().min('2').required('Please enter your name!'),
+        size: yup.string().oneOf(['small', 'medium', 'large'], 'You must select a size!'),
         hasPepperoni: yup.boolean().notRequired(),
         hasXCheese: yup.boolean().notRequired(),
         hasAnchovy: yup.boolean().notRequired(),
@@ -48,19 +43,30 @@ export default function PizzaForm () {
     const validateChange = (event) => {
         yup
             .reach(formSchema, event.target.name)
-            .validate(event.target.name === event.target.value)
-            .then(valid => setErrorState({...errorState, [event.target.name]: ''}))
-            .catch(error => {
-                console.log('Please fix these validation errors:', errorState)
-                setErrorState({...errorState, [event.target.name]:error.errors[0]})
+            .validate(event.target.value)
+            .then(valid => {
+                setErrorState({...errorState, [event.target.name]: ''})
+                console.log(`passState after change with ${event.target.name}`, passState)
             })
+            .catch(error => {
+                setErrorState({...errorState, [event.target.name]:error.errors[0]})
+                console.log('Please fix these validation errors:', errorState)
+            })
+    }
+
+//change handler
+    const handleChanges = (event) => { 
+        event.persist() 
+        validateChange(event)
+        setFormState({...formState, [event.target.name]:event.target.type === 'checkbox' ? event.target.checked : event.target.value})
+        console.log('input changed!', formState)
     }
 
 //check entire form validity
     useEffect(() => {
         console.log('useEffect called')
         formSchema.isValid(formState)
-            .then(validity => setBtnDisabled(validity))
+            .then(validity => setBtnDisabled(!validity))
     }, [formState])
 
     return (
@@ -69,33 +75,61 @@ export default function PizzaForm () {
             <form onSubmit={ submitOrder }>
                 <label htmlFor='name'>Name:
                 {/* must be at least 2 characters */}
-                    <input id='name' name='name' type='text' onChange={ handleChanges } value={ formState.name }/>
+                    <input id='name' 
+                        name='name' 
+                        type='text' 
+                        onChange={ handleChanges } 
+                        value={ formState.name }/>
+                    {errorState.name.length > 0 ? <p className='errorMsg'>{errorState.name}</p> : null}
                 </label>
                 <label htmlFor='size'>Pizza Size:
-                    <select id='size' name='size' value={formState.size} onChange={ handleChanges }>
+                    <select id='size' 
+                        name='size' 
+                        value={formState.size} 
+                        onChange={ handleChanges }>
                         <option value=''>-- Select a Size --</option>
                         <option value='small'>Small - 14in</option>
                         <option value='medium'>Medium - 16in</option>
                         <option value='large'>Large - 18in</option>
                     </select>
+                    {errorState.size.length > 0 ? <p className='errorMsg'>{errorState.size}</p> : null}
                 </label>
                 <section>
                     <p>Toppings:</p>
                     <label htmlFor='hasXCheese'>Extra Cheese
-                        <input id='hasXCheese' name='hasXCheese' type='checkbox'  checked={ formState.hasXCheese } onChange={ handleChanges }/>
+                        <input id='hasXCheese' 
+                            name='hasXCheese' 
+                            type='checkbox'  
+                            checked={ formState.hasXCheese } 
+                            onChange={ handleChanges }/>
                     </label>
                     <label htmlFor='hasPepperoni'>Pepperoni
-                        <input id='hasPepperoni' name='hasPepperoni' type='checkbox' checked={ formState.hasPepperoni }  onChange={ handleChanges }/>
+                        <input id='hasPepperoni' 
+                            name='hasPepperoni' 
+                            type='checkbox' 
+                            checked={ formState.hasPepperoni }  
+                            onChange={ handleChanges }/>
                     </label>
                     <label htmlFor='hasPineapple'>Pineapple
-                        <input id='hasPineapple' name='hasPineapple' type='checkbox' checked={ formState.hasPineapple } onChange={ handleChanges }/>
+                        <input id='hasPineapple'
+                            name='hasPineapple' 
+                            type='checkbox' 
+                            checked={ formState.hasPineapple } 
+                            onChange={ handleChanges }/>
                     </label>   
                     <label htmlFor='hasAnchovy'>Anchovies
-                        <input id='hasAnchovy' name='hasAnchovy' type='checkbox' checked={ formState.hasAnchovy } onChange={ handleChanges }/>
-                    </label>   
+                        <input id='hasAnchovy' 
+                            name='hasAnchovy' 
+                            type='checkbox' 
+                            checked={ formState.hasAnchovy } 
+                            onChange={ handleChanges }/>
+                        </label>   
                 </section>
                 <label htmlFor='special'>Special Instructions:
-                    <textarea id='special' name='special' value={ formState.special } onChange={ handleChanges }/>
+                    <textarea id='special' 
+                        name='special' 
+                        value={ formState.special } 
+                        onChange={ handleChanges }/>
                 </label>
                 <button disabled={ btnDisabled }>Place Order</button>
             </form>
